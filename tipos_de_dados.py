4ceb9f6eaedc2f6571de08885c45e4f36ada8e03
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2025-10-13 01:15:00 -0300

    first commit

---
+++ tipos_de_dados.py
@@ -0,0 +1,9 @@
+print(11 + 10000000)
+print(1.5 + 1 + 2.6)
+print("Domingo boraaaa")
+print(True)
+print(False)
+
+int()
+bool()
+str()
---
+++ variaveis_constantes.py
@@ -0,0 +1,18 @@
+nome = "Adriana"
+idade = 36
+
+nome = "Franciele"
+
+nome, idade = "Tais", 35
+
+nome, idade = ("Carla", 30)
+
+print(nome, idade)
+
+limite_saque_diario = 2000
+
+BRAZILIAN_STATES = ["PR", "SC", "RS", "CE"]
+
+# BRAZILIAN_STATES = 125
+
+print(BRAZILIAN_STATES)
