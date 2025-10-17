a82210a4b9a3aca9a10add884988c0f099bbe00b
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2025-10-17 15:23:03 -0300

    aula prática

---
+++ convertendo_tipos.py
@@ -0,0 +1,18 @@
+print(int(1.92525252))
+print(int("10"))
+print(float("10.10"))
+print(float(100))
+
+print(type(str(10.10)))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print( 100 / 2)
+print(100 // 2)
+
+#float("a")
+
+print(5 // 2)
