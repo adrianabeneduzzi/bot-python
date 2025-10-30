b1fb3accf0dee390cdc303224c12fa8b8a2e2c9a
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2025-10-30 15:01:34 -0300

    aula prática

---
+++ operadores_associacao.py
@@ -0,0 +1,7 @@
+frutas = ["limão", "uva", "ola", "chuva", "qualidade", "laranja"]
+curso = "Curso de python"
+
+print("laranja" in frutas)
+print("limão" not in frutas)
+print("Python" in curso)
+print("python" in curso)
---
+++ operadores_identidade.py
@@ -0,0 +1,16 @@
+# Exemplo 1
+saldo = 1000
+limite = 500
+
+print(saldo is limite)
+print(saldo is not limite)
+
+#Exemplo 2
+saldo = 1000
+limite = 1000
+
+print(saldo is limite)
+print(saldo is not limite)
+
+x = (22 - 10) * 3
+print(x)
