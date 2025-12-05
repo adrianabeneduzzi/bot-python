fbeb00fdc2dc31573115bf70201dce427536cb87
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2025-12-05 00:41:58 -0300

    Adiciona função mensagem_longa e ajusta a execução da função mensagem

--- Decoradores,Iteradores e Geradores/passagem_param.py
+++ Decoradores,Iteradores e Geradores/passagem_param.py
@@ -2,6 +2,13 @@ def mensagem(nome):
     print("executando mensagem")
     return f"Oi {nome}"
 
-def mensagem(nome):
+def mensagem_longa(nome):
     print("executando mensagem longa")
     return f"Olá tudo bem com você {nome}?"
+
+def executar(funcao):
+    print("executando executar")
+    return funcao("João")
+
+
+print(executar(mensagem))
