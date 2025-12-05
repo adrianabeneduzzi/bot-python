5ee16240f83d9df2cb3f13fad044a176569ec59f
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2025-12-04 23:59:42 -0300

    Adiciona funções de mensagem com diferentes saudações

---
+++ Decoradores,Iteradores e Geradores/passagem_param.py
@@ -0,0 +1,7 @@
+def mensagem(nome):
+    print("executando mensagem")
+    return f"Oi {nome}"
+
+def mensagem(nome):
+    print("executando mensagem longa")
+    return f"Olá tudo bem com você {nome}?"
