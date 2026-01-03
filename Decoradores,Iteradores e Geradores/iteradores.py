c0e474b19b397b1c8ed23f83e31663721d5f6080
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2026-01-03 00:50:59 -0300

    Implementa a classe MeuIterador com iteração e multiplicação de elementos

--- Decoradores,Iteradores e Geradores/iteradores.py
+++ Decoradores,Iteradores e Geradores/iteradores.py
@@ -1,5 +1,18 @@
 class MeuIterador:
-    pass
+    def __init__(self, numeros: list[int]):
+        self.numeros = numeros
+        self.contador = 0
 
-for i in MeuIterador():
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            numero = self.numeros[self.contador]
+            self.contador += 1
+            return numero * 2
+        except IndexError:
+            raise StopIteration
+
+for i in MeuIterador(numeros=[25, 4, 58]):
     print(i)
